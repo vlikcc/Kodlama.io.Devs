e147947de4c632197c28f48e954fb276e16c3766
vlikcc <vli.kcc@hotmail.com>
2022-09-01 13:41:53 +0000

    01.09.2022

---
+++ src/kodlama.io.Devs/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+                                                                IConfiguration configuration)
+        {
+            services.AddDbContext<BaseDbContext>(options =>
+                                                     options.UseSqlServer(
+                                                         configuration.GetConnectionString("kodlama.ioConnectionString")));
+            services.AddScoped<IProgrammingLanguageRepository, ProgrammingLanguageRepository>();
+
+            return services;
+        }
+    }
+}
+
