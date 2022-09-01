71db823f736daa0106c1cb742ca20cd66f91c368
vlikcc <vli.kcc@hotmail.com>
2022-09-01 15:55:50 +0300

    Add project files.

---
+++ src/kodlama.io.Devs/Application/Features/ProgrammingLanguages/Commands/CommandValidators/CreateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.Features.ProgrammingLanguages.Commands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.CommandValidators
+{
+    public class CreateProgrammingLanguageCommandValidator:AbstractValidator<CreateProgrammingLanguage>
+    {
+        public CreateProgrammingLanguageCommandValidator()
+        {
+            RuleFor(p=>p.Name).NotEmpty();
+        }
+    }
+}
---
+++ src/kodlama.io.Devs/Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage.cs
@@ -0,0 +1,43 @@
+﻿using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Features.ProgrammingLanguages.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Commands
+{
+    public partial class CreateProgrammingLanguage:IRequest<CreatedProgrammingLanguageDto>
+    {
+        public string Name { get; set; }
+        public class CreateProgrammingLanguageCommandHandler : IRequestHandler<CreateProgrammingLanguage, CreatedProgrammingLanguageDto>
+        {
+            private readonly IProgrammingLanguageRepository _repository;
+            private readonly IMapper _mapper;
+            private readonly ProgrammingLanguageBusinessRules _businessRules;
+            public CreateProgrammingLanguageCommandHandler(IProgrammingLanguageRepository languageRepository, IMapper mapper, ProgrammingLanguageBusinessRules businessRules)
+            {
+                _repository = languageRepository;
+                _mapper = mapper;
+                _businessRules = businessRules;
+            }
+            public async Task<CreatedProgrammingLanguageDto> Handle(CreateProgrammingLanguage request, CancellationToken cancellationToken)
+            {
+                await _businessRules.ProgrammingLanguageNameCannotBeDublicatedWhenInserted(request.Name);
+                ProgrammingLanguage mappedLanguage = _mapper.Map<ProgrammingLanguage>(request.Name);
+                ProgrammingLanguage createdLanguage = await _repository.AddAsync(mappedLanguage);
+                CreatedProgrammingLanguageDto createdProgrammingLanguageDto = _mapper.Map<CreatedProgrammingLanguageDto>(createdLanguage);
+                return createdProgrammingLanguageDto;
+
+
+            }
+        }
+
+    }
+}
