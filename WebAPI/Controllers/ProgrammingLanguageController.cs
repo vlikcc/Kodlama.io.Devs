ed47a05c97a9d7068aea1f9c667a115de1d2e6e9
vlikcc <vli.kcc@hotmail.com>
2022-09-07 14:25:27 +0300

    07.09.2022 3.Commit Homework Finished

--- WebAPI/Controllers/ProgrammingLanguageController.cs
+++ WebAPI/Controllers/ProgrammingLanguageController.cs
@@ -17,7 +17,7 @@ namespace WebAPI.Controllers
     public class ProgrammingLanguageController : BaseController
     {
         [HttpPost]
-         public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
         {
             CreatedProgrammingLanguageDto result = await Mediator.Send(createProgrammingLanguageCommand);
             return Created("", result);
@@ -27,27 +27,27 @@ namespace WebAPI.Controllers
         public async Task<IActionResult> Delete([FromBody] DeleteProgrammingLanguageCommand deleteProgrammingLanguageCommand)
         {
             DeletedProgrammingLanguageDto result = await Mediator.Send(deleteProgrammingLanguageCommand);
-            return Ok ( result);
+            return Ok(result);
         }
 
         [HttpPost("Update")]
-        
-        public async Task <IActionResult> Update([FromBody] UpdateProgrammingLanguageCommand updateProgrammingLanguageCommand)
+
+        public async Task<IActionResult> Update([FromBody] UpdateProgrammingLanguageCommand updateProgrammingLanguageCommand)
         {
             UpdatedProgrammingLanguageDto result = await Mediator.Send(updateProgrammingLanguageCommand);
             return Ok(result);
         }
 
-        [HttpGet]         
-        public async Task<IActionResult> GetList([FromQuery]  PageRequest pageRequest)
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
         {
             GetListProgrammingLanguageQuery getListProgrammingLanguageQuery = new() { PageRequest = pageRequest };
             ProgrammingLanguageListModel result = await Mediator.Send(getListProgrammingLanguageQuery);
-            return Ok(result);                 
-            
+            return Ok(result);
+
         }
 
-        [HttpGet("Id")]
+        [HttpGet("{Id}")]
         public async Task<IActionResult> GetById([FromRoute] GetByIdProgrammingLanguageQuery getByIdProgrammingLanguageQuery)
         {
             ProgrammingLanguageGetByIdDto result = await Mediator.Send(getByIdProgrammingLanguageQuery);
